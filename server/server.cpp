1a1c603d22c9cad21e9bf720585a08429f39f679
Mikhail Baranov <baranov.mv@gmail.com>
2025-10-12 23:26:44 +0200

    New attempt

--- server/server.cpp
+++ server/server.cpp
@@ -20,9 +20,18 @@
 #define FILE_OUT ("/var/tmp/aesdsocketdata")
 
 std::atomic_int running = 1;
-static int serve_client(int server_fd, struct sockaddr_in *address, FILE *fout);
+std::atomic_int waiting_for_connection = 0;
 
-void sig_handler(int sgnum) { running = 0; }
+static int serve_client(int server_fd, struct sockaddr_in *address);
+
+void sig_handler(int sgnum) {
+  running = 0;
+  // If we're blocked in accept(), we need to handle cleanup
+  if (waiting_for_connection) {
+    syslog(LOG_USER, "Caught signal, exiting");
+  }
+}
+
 void sig_setup() {
   struct sigaction sa;
   sa.sa_handler = sig_handler;
@@ -54,6 +63,12 @@ void daemonize() {
     exit(EXIT_SUCCESS);
   }
 
+  // Create new session
+  if (setsid() < 0) {
+    perror("setsid failed");
+    exit(EXIT_FAILURE);
+  }
+
   // Change working directory to root
   if (chdir("/") < 0) {
     perror("chdir failed");
@@ -61,7 +76,7 @@ void daemonize() {
   }
 
   // Redirect stdin, stdout, stderr to /dev/null
-  int fd = open("/dev/null", O_WRONLY | O_TRUNC | O_CREAT, 0644);
+  int fd = open("/dev/null", O_RDWR);
   if (fd < 0) {
     perror("Cannot open /dev/null");
     exit(EXIT_FAILURE);
@@ -69,7 +84,9 @@ void daemonize() {
     dup2(fd, STDIN_FILENO);
     dup2(fd, STDOUT_FILENO);
     dup2(fd, STDERR_FILENO);
-    close(fd);
+    if (fd > 2) {
+      close(fd);
+    }
   }
 }
 
@@ -95,17 +112,19 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
-                 sizeof(opt))) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
     perror("setsockopt");
+    close(server_fd);
     return EXIT_FAILURE;
   }
+
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(9000);
 
   if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
     perror("bind failed");
+    close(server_fd);
     return EXIT_FAILURE;
   }
 
@@ -113,111 +132,150 @@ int main(int argc, char *argv[]) {
   if (daemon_mode) {
     daemonize();
     // Reopen syslog after daemonizing since file descriptors were closed
-    openlog("server", 0, LOG_USER);
+    openlog("aesdsocket", 0, LOG_USER);
   }
 
-  FILE *fout = fopen(FILE_OUT, "w+");
-  if (!fout) {
-    perror("Cannot open file");
-    close(server_fd);
-    return EXIT_FAILURE;
-  }
-
-  if (listen(server_fd, 5) < 0 && errno != EINTR) {
+  if (listen(server_fd, 5) < 0) {
     perror("listen");
-    fclose(fout);
     close(server_fd);
     return EXIT_FAILURE;
-  } else if (errno == EINTR) {
-    goto STOP;
   }
 
   do {
-    ret = serve_client(server_fd, &address, fout);
+    ret = serve_client(server_fd, &address);
     if (ret < 0) {
       break;
     }
   } while (running);
 
-STOP:
   close(server_fd);
-  fclose(fout);
   remove(FILE_OUT);
-  return ret;
+  closelog();
+  return ret < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
-int serve_client(int server_fd, struct sockaddr_in *address, FILE *fout) {
+int serve_client(int server_fd, struct sockaddr_in *address) {
   ssize_t pkt_len = 0;
   std::string buffer;
   size_t buffer_idx = 0;
   char pkt[2048] = {0};
-  size_t file_sz = 0;
   socklen_t addrlen = sizeof(*address);
   int new_socket;
-  if (!fout) {
-    perror("Cannot open file");
-    return EXIT_FAILURE;
-  }
-  if ((new_socket = accept(server_fd, (struct sockaddr *)address, &addrlen)) <
-          0 &&
-      errno != EINTR) {
+  int fd = -1;
+
+  waiting_for_connection = 1;
+  new_socket = accept(server_fd, (struct sockaddr *)address, &addrlen);
+  waiting_for_connection = 0;
+
+  if (new_socket < 0) {
+    if (errno == EINTR || running == 0) {
+      return 0; // Signal received, normal exit
+    }
     perror("accept");
     return EXIT_FAILURE;
-  } else if (errno == EINTR) {
-    goto STOP;
   }
+
   inet_ntop(AF_INET, &(address->sin_addr), pkt, INET_ADDRSTRLEN);
   syslog(LOG_USER, "Accepted connection from %s", pkt);
 
+  // Open file for this connection
+  fd = open(FILE_OUT, O_RDWR | O_CREAT | O_APPEND, 0644);
+  if (fd < 0) {
+    perror("Cannot open file");
+    close(new_socket);
+    return EXIT_FAILURE;
+  }
+
   while (running) {
-    pkt_len = recv(new_socket, pkt, sizeof(pkt) / sizeof(pkt[0]) - 1, 0);
-    if (running == 0 || pkt_len == 0) {
-      goto STOP;
+    pkt_len = recv(new_socket, pkt, sizeof(pkt) - 1, 0);
+
+    if (pkt_len == 0 || running == 0) {
+      // Connection closed or signal received
+      break;
     } else if (pkt_len < 0) {
+      if (errno == EINTR) {
+        break;
+      }
       perror("Error while recv");
-      goto STOP;
-    } else {
-      buffer.insert(buffer.end(), pkt, pkt + pkt_len);
-      for (size_t i = buffer_idx; i < buffer.length(); ++i) {
-        if (buffer[i] == '\n') {
-          fwrite(&buffer[buffer_idx], 1, i - buffer_idx + 1, fout);
-          fflush(fout);
-          fseek(fout, 0, SEEK_SET);
-          clearerr(fout);
-          buffer_idx = i + 1;
-          do {
-            char buff_read[1500];
-            const size_t ret_code = fread(
-                buff_read, 1, sizeof(buff_read) / sizeof(buff_read[0]), fout);
-            if (ret_code > 0) {
-              size_t sent = 0;
-              do {
-                const int send_ret =
-                    send(new_socket, buff_read + sent, ret_code - sent, 0);
-                if (send_ret < 0) {
-                  perror("Can't send");
-                  return EXIT_FAILURE;
-                } else if (send_ret == 0) {
-                  break;
-                }
-                sent += send_ret;
-              } while (sent < ret_code);
+      break;
+    }
+
+    // Append received data to buffer
+    buffer.insert(buffer.end(), pkt, pkt + pkt_len);
+
+    // Process all complete packets (ending with \n)
+    for (size_t i = buffer_idx; i < buffer.length(); ++i) {
+      if (buffer[i] == '\n') {
+        // Write this packet to file
+        ssize_t write_len = i - buffer_idx + 1;
+        if (write(fd, &buffer[buffer_idx], write_len) != write_len) {
+          perror("Error writing to file");
+          close(fd);
+          close(new_socket);
+          return EXIT_FAILURE;
+        }
+
+        buffer_idx = i + 1;
+
+        // Close and reopen file for reading
+        close(fd);
+        fd = open(FILE_OUT, O_RDONLY);
+        if (fd < 0) {
+          perror("Cannot open file for reading");
+          close(new_socket);
+          return EXIT_FAILURE;
+        }
+
+        // Send entire file contents back to client
+        char buff_read[1500];
+        ssize_t bytes_read;
+        while ((bytes_read = read(fd, buff_read, sizeof(buff_read))) > 0) {
+          size_t sent = 0;
+          while (sent < (size_t)bytes_read) {
+            ssize_t send_ret =
+                send(new_socket, buff_read + sent, bytes_read - sent, 0);
+            if (send_ret < 0) {
+              perror("Can't send");
+              close(fd);
+              close(new_socket);
+              return EXIT_FAILURE;
+            } else if (send_ret == 0) {
+              break;
             }
-          } while (!feof(fout));
+            sent += send_ret;
+          }
+        }
+
+        // Close and reopen file for appending
+        close(fd);
+        fd = open(FILE_OUT, O_RDWR | O_CREAT | O_APPEND, 0644);
+        if (fd < 0) {
+          perror("Cannot open file for appending");
+          close(new_socket);
+          return EXIT_FAILURE;
         }
       }
-      file_sz += pkt_len;
-      pkt_len = 0;
+    }
+
+    // Clean up processed data from buffer to prevent unbounded growth
+    if (buffer_idx > 0) {
+      buffer.erase(0, buffer_idx);
+      buffer_idx = 0;
     }
   }
 
-STOP:
+  // Cleanup
+  if (fd >= 0) {
+    close(fd);
+  }
+
   if (running == 0) {
     syslog(LOG_USER, "Caught signal, exiting");
-  } else if (pkt_len == 0) {
+  } else {
     inet_ntop(AF_INET, &(address->sin_addr), pkt, INET_ADDRSTRLEN);
     syslog(LOG_USER, "Closed connection from %s", pkt);
   }
+
   close(new_socket);
-  return pkt_len;
+  return 0;
 }
